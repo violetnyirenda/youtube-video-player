f01c91b9cc786f34fde4c4ea422faa95e551afe6
Violet Nyirenda <nyirendavviolet728@gmail.com>
2024-01-28 13:43:09 +0400

    variable name change

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 import random
 
+
 app = Flask(__name__)
 
-words = ["monkey", "lion", "", "computer", "game"]
-chosen_word = random.choice(words).lower()
+animals = ["monkey", "lion", "zebra", "dog", "tiger"]
+chosen_animal = random.choice(animals).lower()
 guessed_letters = set()
 attempts = 6
 
-@app.route('/hangman', methods=['POST'])
-def hangman():
+@app.route('/genius', methods=['POST'])
+def animals():
     global attempts, guessed_letters
 
     data = request.get_json()
@@ -24,10 +25,10 @@ def hangman():
 
     guessed_letters.add(guess)
 
-    if guess not in chosen_word:
+    if guess not in chosen_animal:
         attempts -= 1
 
-    game_state = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_word)
+    game_state = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_animal)
 
     response = {
         'attempts_left': attempts,
@@ -35,9 +36,9 @@ def hangman():
     }
 
     if '_' not in game_state:
-        response['message'] = 'Congratulations! You guessed the word.'
+        response['message'] = 'Congratulations! You guessed the animal.'
     elif attempts == 0:
-        response['message'] = f'Sorry, you ran out of attempts. The word was: {chosen_word}'
+        response['message'] = f'Sorry, you ran out of attempts. The animal was: {chosen_animal}'
 
     return jsonify(response)
 
